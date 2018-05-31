6dd5ef40829f4fc247ba61de9c3aa02e6f4b3904
Cameron Sweeney <cp_sweeney@hotmail.com>
2018-05-31 13:24:19 -0700

    Format models to be same as Autotask entities

--- djautotask/models.py
+++ djautotask/models.py
@@ -1,19 +1,12 @@
-import re
-import logging
-
 from django.db import models
 from django_extensions.db.models import TimeStampedModel
 
 
-logger = logging.getLogger(__name__)
-
-
-PRIORITY_RE = re.compile('^Priority ([\d]+)')
-
-
 class Account(models.Model):
-    name = models.CharField(blank=True, null=True, max_length=100)
-    number = models.CharField(blank=True, null=True, max_length=50)
+    account_name = models.CharField(blank=True, null=True, max_length=100)
+    account_number = models.CharField(blank=True, null=True, max_length=50)
+    account_type = models.IntegerField(blank=True, null=True)
+    active = models.NullBooleanField(blank=True)
     account_id = models.IntegerField(blank=True, null=True)
     last_activity_date = models.DateField(blank=True, null=True)
     phone_number = models.CharField(blank=True, null=True, max_length=250)
@@ -29,136 +22,121 @@ class Account(models.Model):
     market = models.CharField(blank=True, null=True, max_length=250)
 
     def __str__(self):
-        return self.name
+        return self.account_name
 
 
 class Project(models.Model):
-    name = models.CharField(blank=True, null=True, max_length=200)
-    last_activity_date = models.DateField(blank=True, null=True)
+    actual_billed_hours = models.DecimalField(
+        blank=True, null=True, decimal_places=2, max_digits=6)
     actual_hours = models.DecimalField(
         blank=True, null=True, decimal_places=2, max_digits=6)
-    budget_hours = models.DecimalField(
+    completed_date_time = models.DateField(blank=True, null=True)
+    create_date_time = models.DateField(blank=True, null=True)
+    department = models.IntegerField(blank=True, null=True)
+    description = models.TextField(blank=True, null=True, max_length=2000)
+    duration = models.IntegerField(blank=True, null=True)
+    end_date_time = models.DateField(blank=True, null=True)
+    estimated_time = models.DecimalField(
         blank=True, null=True, decimal_places=2, max_digits=6)
-    scheduled_hours = models.DecimalField(
-        blank=True, null=True, decimal_places=2, max_digits=6)
+    project_name = models.CharField(blank=True, null=True, max_length=100)
+    project_number = models.CharField(blank=True, null=True, max_length=50)
+    start_date_time = models.DateField(blank=True, null=True)
+    status = models.IntegerField(blank=True, null=True)
+    status_date_time = models.DateField(blank=True, null=True)
+    status_detail = models.TextField(blank=True, null=True, max_length=2000)
+    project_type = models.IntegerField(blank=True, null=True)
 
     account_id = models.ForeignKey(
-        'Account',
-        blank=True,
-        null=True,
-        on_delete=models.SET_NULL
-    )
+        'Account', blank=True, null=True, on_delete=models.SET_NULL)
+    creator_resource_id = models.ForeignKey(
+        'Resource', blank=True, null=True, on_delete=models.SET_NULL)
 
     def __str__(self):
         return self.name
 
 
 class Ticket(models.Model):
-    RECORD_TYPES = (
-        ('ServiceTicket', "Service Ticket"),
-        ('ProjectTicket', "Project Ticket"),
-        ('ProjectIssue', "Project Issue"),
-    )
-    actual_hours = models.DecimalField(
+    # Main Autotask API ticket fields
+    creator_resource_id = models.IntegerField(blank=True, null=True)
+    completed_date = models.DateTimeField(blank=True, null=True)
+    description = models.TextField(blank=True, null=True, max_length=8000)
+    due_date_time = models.DateTimeField(blank=True, null=True)
+    estimated_hours = models.DecimalField(
         blank=True, null=True, decimal_places=2, max_digits=6)
-    agreement_id = models.IntegerField(blank=True, null=True)
-    approved = models.NullBooleanField(blank=True, null=True)
-    budget_hours = models.DecimalField(
+    hours_to_be_scheduled = models.DecimalField(
         blank=True, null=True, decimal_places=2, max_digits=6)
-    closed_by = models.CharField(blank=True, null=True, max_length=250)
-    closed_date_utc = models.DateTimeField(blank=True, null=True)
-    closed_flag = models.NullBooleanField(blank=True, null=True)
-    customer_updated = models.BooleanField(default=False)
-    date_resolved_utc = models.DateTimeField(blank=True, null=True)
-    date_resplan_utc = models.DateTimeField(blank=True, null=True)
-    date_responded_utc = models.DateTimeField(blank=True, null=True)
-    entered_date_utc = models.DateTimeField(blank=True, null=True)
-    has_child_ticket = models.NullBooleanField()
-    impact = models.CharField(blank=True, null=True, max_length=250)
-    is_in_sla = models.NullBooleanField(blank=True, null=True)
-    last_updated_utc = models.DateTimeField(blank=True, null=True)
-    parent_ticket_id = models.IntegerField(blank=True, null=True)
-    record_type = models.CharField(blank=True, null=True,
-                                   max_length=250, choices=RECORD_TYPES,
-                                   db_index=True)
-    required_date_utc = models.DateTimeField(blank=True, null=True)
-    respond_mins = models.IntegerField(blank=True, null=True)
-    resolve_mins = models.IntegerField(blank=True, null=True)
-    resources = models.CharField(blank=True, null=True, max_length=250)
-    res_plan_mins = models.IntegerField(blank=True, null=True)
-    severity = models.CharField(blank=True, null=True, max_length=250)
-    site_name = models.CharField(blank=True, null=True, max_length=250)
-    source = models.CharField(blank=True, null=True, max_length=250)
-    sub_type = models.CharField(blank=True, null=True, max_length=250)
-    sub_type_item = models.CharField(blank=True, null=True, max_length=250)
-    summary = models.CharField(blank=True, null=True, max_length=250)
-    updated_by = models.CharField(blank=True, null=True, max_length=250)
+    last_activity_date = models.DateTimeField(blank=True, null=True)
+    opportunity_id = models.IntegerField(blank=True, null=True)
+    priority = models.IntegerField(blank=True, null=True)
+    resolution = models.TextField(blank=True, null=True, max_length=32000)
+    resolution_plan_due_date = models.DateTimeField(blank=True, null=True)
+    resolution_plan_due_date_time = models.DateTimeField(blank=True, null=True)
+    resolved_date_time = models.DateTimeField(blank=True, null=True)
+    resolved_due_date_time = models.DateTimeField(blank=True, null=True)
+    sla_been_met = models.NullBooleanField(blank=True, null=True)
+    sla_id = models.IntegerField(blank=True, null=True)
+    source = models.IntegerField(blank=True, null=True)
+    status = models.IntegerField(blank=True, null=True)
+    ticket_category = models.IntegerField(blank=True, null=True)
+    ticket_number = models.CharField(blank=True, null=True, max_length=50)
+    ticket_type = models.IntegerField(blank=True, null=True)
+    title = models.CharField(blank=True, null=True, max_length=255)
 
-    """
-    board = models.ForeignKey(
-        'AutotaskBoard', blank=True, null=True, on_delete=models.CASCADE)
-    account = models.ForeignKey(
+    account_id = models.ForeignKey(
         'Account', blank=True, null=True, related_name='account_tickets',
         on_delete=models.SET_NULL)
-    location = models.ForeignKey(
-        'Location', blank=True, null=True, related_name='location_tickets',
+    assigned_resource_id = models.ForeignKey(
+        'Resource', blank=True, null=True,
+        related_name='assigned_resource_tickets',
         on_delete=models.SET_NULL)
-    # Leave out for now
-    members = models.ManyToManyField(
-        'Member', through='ScheduleEntry',
-        related_name='member_tickets')
-    owner = models.ForeignKey(
-        'Member', blank=True, null=True, on_delete=models.SET_NULL)
-    priority = models.ForeignKey(
-        'TicketPriority', blank=True, null=True, on_delete=models.SET_NULL)
-    project = models.ForeignKey(
-        'Project', blank=True, null=True, related_name='project_tickets',
+    assigned_resource_role_id = models.ForeignKey(
+        'Role', blank=True, null=True, related_name='role_tickets',
+        on_delete=models.SET_NULL)
+    creator_resource_id = models.ForeignKey(
+        'Resource', blank=True, null=True,
+        related_name='creator_resource_tickets',
+        on_delete=models.SET_NULL)
+    project_id = models.ForeignKey(
+        'Project', blank=True, null=True,
+        related_name='project_tickets',
         on_delete=models.CASCADE)
-    status = models.ForeignKey(
-        'BoardStatus', blank=True, null=True, related_name='status_tickets',
-        on_delete=models.SET_NULL)
-    team = models.ForeignKey(
-        'Team', blank=True, null=True, related_name='team_tickets',
-        on_delete=models.SET_NULL)
-    """
+    # Might need to include, board, priority, status and
+    # team as in django-connectwise
 
     class Meta:
         verbose_name = 'Ticket'
         verbose_name_plural = 'Tickets'
-        ordering = ('summary', )
+        ordering = ('title', )
 
     def __str__(self):
         return '{}-{}'.format(self.id, self.summary)
 
 
-class TicketNote(TimeStampedModel):
+class TicketCategory(models.Model):
     pass
-    """
-    created_by = models.TextField(blank=True, null=True, max_length=250)
-    resource_id = models.IntegerField(blank=True, null=True)
-    date_created = models.DateTimeField(blank=True, null=True)
-    #makemigrations complains about BooleanField being non nullable
-    #detail_description_flag = models.BooleanField(blank=True)
-    #external_flag = models.BooleanField(blank=True)
-    #internal_analysis_flag = models.BooleanField(blank=True)
-    #internal_flag = models.BooleanField(blank=True)
-    #resolution_flag = models.BooleanField(blank=True)
-    description = models.TextField(blank=True, null=True, max_length=2000)
 
-    ticket = models.ForeignKey('Ticket', on_delete=models.CASCADE)
-    member = models.ForeignKey(
-        'Member', blank=True, null=True, on_delete=models.SET_NULL)
+
+class TicketNote(TimeStampedModel):
+    description = models.TextField(blank=True, null=True, max_length=3200)
+    last_activity_date = models.DateTimeField(blank=True, null=True)
+    note_type = models.IntegerField(blank=True, null=True)
+    publish = models.IntegerField(blank=True, null=True)
+    title = models.CharField(blank=True, null=True, max_length=250)
+
+    creator_resource_id = models.ForeignKey(
+        'Resource', blank=True, null=True, on_delete=models.SET_NULL)
+    ticket_id = models.ForeignKey(
+        'Ticket', on_delete=models.CASCADE)
 
     class Meta:
-        ordering = ('-date_created', 'id')
         verbose_name_plural = 'Notes'
 
     def __str__(self):
-        return 'Ticket {} note: {}'.format(self.ticket, str(self.date_created))
-"""
+        return 'Ticket {} note: {}'.format(self.ticket,
+                                           str(self.last_activity_date))
 
 
-class TicketCategory(models.Model):
+class TicketTimeEntry(models.Model):
     pass
 
 
@@ -166,23 +144,7 @@ class TicketSecondaryResource(models.Model):
     pass
 
 
-class TicketPriority(models.Model):
-    pass
-
-
 class Resource(models.Model):
-    pass
-
-
-class Location(models.Model):
-    pass
-
-
-class Member(models.Model):
-    pass
-
-
-class BoardStatus(models.Model):
     pass
 
 
@@ -191,6 +153,10 @@ class Team(models.Model):
 
 
 class AutotaskBoard(models.Model):
+    pass
+
+
+class Role(models.Model):
     pass
 
 
