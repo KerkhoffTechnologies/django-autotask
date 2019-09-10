4f2debcfa9deab015abaffc4003a7783ef8fc4e0
Cameron Sweeney <cameron@craftypenguins.net>
2019-09-10 10:11:58 -0700

    Add test sync all

--- djautotask/tests/fixture_utils.py
+++ djautotask/tests/fixture_utils.py
@@ -27,6 +27,9 @@ def generate_objects(object_type, fixture_objects):
     """
     Generate multiple objects based on the given fixtures.
     """
+    # Access a copy of the Autotask API's WSDL file from the tests directory
+    # so that we can generate mock objects from the API without actually
+    # calling the API.
     path = os.path.abspath("djautotask/tests/atws.wsdl")
     url = urllib.parse.urljoin('file:', urllib.request.pathname2url(path))
     client = Client(url)
