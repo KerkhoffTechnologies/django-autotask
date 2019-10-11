7f0c6b079e2b61cbd9df5cae8a7e38c06ffe2e34
swolfe <sam@kerkhofftech.ca>
2019-10-11 09:04:25 -0700

    Added comments for update method

--- djautotask/api.py
+++ djautotask/api.py
@@ -90,6 +90,13 @@ class AutotaskRequestsTransport(transport.Transport):
 
 
 def update_ticket(ticket, status):
+    # We need to query for the object first, then alter it and execute it.
+    # https://atws.readthedocs.io/usage.html#querying-for-entities
+
+    # This is because we can not create a valid (enough) object to update
+    # to autotask unless we sync EVERY non-readonly field. If you submit the
+    # object with no values supplied for the readonly fields, autotask will
+    # null them out.
     query = Query('Ticket')
     query.WHERE('id', query.Equals, ticket.id)
     at = init_api_connection()
