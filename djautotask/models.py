432519b74127f25c7d0b1c63fd5d44d50b7a18e8
Matt Fox <matt@topleft.team>
2024-06-26 14:17:39 -0700

    Fix whitespace

--- djautotask/models.py
+++ djautotask/models.py
@@ -242,8 +242,8 @@ class ProjectStatus(Picklist):
         verbose_name_plural = 'Project statuses'
 
     def is_complete(self):
-      return self.id == ProjectStatus.COMPLETE_ID 
-         
+        return self.id == ProjectStatus.COMPLETE_ID
+
 
 class DisplayColor(Picklist):
 
