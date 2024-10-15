862ca96a2063ba32bc61e261aaf7ff373d7d2ed2
Sam Wolfe <sam@topleft.team>
2024-10-15 09:59:35 -0700

    fix formatting

--- djautotask/__init__.py
+++ djautotask/__init__.py
@@ -4,7 +4,6 @@ VERSION = (1, 6, 5, 'final')
 # pragma: no cover
 if VERSION[-1] != "final":
     __version__ = '.'.join(map(str, VERSION))
-    
 else:
     # pragma: no cover
     __version__ = '.'.join(map(str, VERSION[:-1]))
