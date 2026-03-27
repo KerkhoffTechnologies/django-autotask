556653b1a5400fd1ae4f89458b007306c8266c6d
zeryab-topleft <zkhan@topleft.team>
2026-03-27 23:03:15 +0500

    update service calls (#254)
    Co-authored-by: Zeryab Khan <zeryabkhan91@gmail.com>

--- djautotask/sync.py
+++ djautotask/sync.py
@@ -1653,7 +1653,7 @@ class TaskPredecessorSynchronizer(
 
 
 class ServiceCallSynchronizer(
-        CreateRecordMixin, BatchQueryMixin, Synchronizer):
+        CreateRecordMixin, UpdateRecordMixin, BatchQueryMixin, Synchronizer):
 
     client_class = api.ServiceCallsAPIClient
     model_class = models.ServiceCallTracker
